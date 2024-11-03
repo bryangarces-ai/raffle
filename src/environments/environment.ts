d368a72281e714ac037e43c89db4ee0b46fc2a45
BRYAN JHONS GARCES <garcesbryan11@gmail.com>
2024-11-03 19:34:52 +0800

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@
 export const environment = {
     production: false,
     //local
-    //apiUrl: 'https://localhost:7021/api',
+    apiUrl: 'https://localhost:7021/api',
     //server
-     apiUrl: 'https://172.16.0.37/api',
+    // apiUrl: 'https://172.16.0.37/api',
     featureFlags: {
       newFeatureEnabled: true,
     },
